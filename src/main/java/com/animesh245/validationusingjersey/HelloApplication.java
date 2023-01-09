c296ae46d499ffa762661d19fb9ba0bfc1a9c154
Animesh245 <bdanimesh245@gmail.com>
2023-01-10 00:39:45 +0600

    Initial Commit

---
+++ src/main/java/com/animesh245/validationusingjersey/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.animesh245.validationusingjersey;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
---
+++ src/main/java/com/animesh245/validationusingjersey/Student.java
@@ -0,0 +1,4 @@
+package com.animesh245.validationusingjersey;
+
+public class Student {
+}
